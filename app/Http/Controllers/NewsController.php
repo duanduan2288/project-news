1f1785380e6b1c94e348950dc56ea39a5a6d9ac1
duan2288@126.com <duan2288@126.com>
2016-07-12 15:47:26 +0800

    asas

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1106,13 +1106,17 @@ class NewsController extends Controller
 	 */
 	public function postWechatmedia(){
 		$media_id = $this->requestData['media_id'];
+
 		$app = $this->return_app();
 		$temporary = $app->material_temporary;
+
 		$content = $temporary->getStream($media_id);
+
 		$ext =    File::getStreamExt($content);
 		$file_name = $media_id.$ext;
 		$outossurl = config('services.aliyun.OutOss');
 		$Bucket = config('services.aliyun.Bucket');
+
 		try{
 			//上传到阿里云
 			$oss = ServiceUpload::boot();
